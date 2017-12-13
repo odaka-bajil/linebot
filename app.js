a8ed6c349a735d2b77ff5260085118ef245ba4b1
unknown <user@odaka-ch.local>
2017-12-13 09:38:44 +0900

    学校からのコミット

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get("/", function(req, res, next){ //追加
 function getMessageText(text) {
   var message = 'こんにちは';
     message = getMessageTextFromCSV(text);
-    return message;//追加
+    //return message;//追加
   if(text.indexOf("モンスト")>=0){
     return'STEART'
   }
